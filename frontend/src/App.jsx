962e29d29721ecac628c813841ed8ae4425f7525
sushant696 <sushantprasaii7@gmail.com>
2024-09-15 09:57:04 +0545

    feat: normal pagination completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MovieList from "./movies";
 
 function App() {
   return (
-    <>
-      <MovieList />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
